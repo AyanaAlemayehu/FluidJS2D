7b95fbb7e148341088727de2fdbe877627c13ebb
AyanaA450 <AyanaA450@gmail.com>
2020-11-05 01:12:25 -0500

    added rudimentary UI for pausing and playing. Making good progress, time to sleep

--- public/calc/particles.js
+++ public/calc/particles.js
@@ -1,12 +1,13 @@
 // WHEN YOU CHANGE START T, YOU STOP THE PARTICLE FROM BEING IN SYNC WITH THE BACKGROUND VECTOR FIELD
 class Particle{
-    constructor(x, y, FInterface, deltaT, radius, startT = 0, trail = false, color = "black"){//START T NEEDA BE IN MILLISECONDS IS ADDED TO WHAT DATE GETTIME GIVES
+    constructor(x, y, FInterface, deltaT, radius, pauser, startT = 0, trail = false, color = "black"){//START T NEEDA BE IN MILLISECONDS IS ADDED TO WHAT DATE GETTIME GIVES
         this.x = x;
         this.y = y;
         this.startT = new Date().getTime();
         this.tAdded = startT;
         this.deltaT = deltaT;//in milliseconds, literally just for setInterval
         this.radius = radius;
+        this.pauser = pauser;
         this.trail = trail;
         this.color = color;
         this.FInterface = FInterface;
@@ -19,12 +20,17 @@ class Particle{
         FInterface.ctxParticle.fill();
     }
     increment(){
-        var x = this.x;
-        var y = this.y;
-        var t = new Date().getTime() - this.startT + this.tAdded;//find out the ABSOLUTE time, then add the startT value the user inputted
-        this.x += this.FInterface.vEqX(x, y, t*.001);//to turn into seconds for equations multiply by .001
-        this.y += this.FInterface.vEqY(x, y, t*.001);//
-        this.drawPart();
+        if (!this.pauser.pause){
+            var x = this.x;
+            var y = this.y;
+            this.t = new Date().getTime() - this.startT + this.tAdded;//find out the ABSOLUTE time, then add the startT value the user inputted
+            this.x += this.FInterface.vEqX(x, y, this.t*.001);//to turn into seconds for equations multiply by .001
+            this.y += this.FInterface.vEqY(x, y, this.t*.001);//
+            this.drawPart();
+        }else if (this.pauser.pause){
+            this.tAdded = this.t;
+            this.startT = new Date().getTime();
+        }
     }
     runInterval(){
         window.setInterval(this.increment.bind(this), this.deltaT);
